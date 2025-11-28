bf5868128f67e95162f981eabf2ff4b7afd421d2
Max Kollmeyer <max.kollmeyer@gmail.com>
2025-11-28 14:55:50 -0500

    Moved backend to server folder and created a folder for the frontend

---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+import express from "express"
+
+const router = express.Router();
+/*
+router.post("/proj", createProj);
+router.get("/proj", getAllProj);
+router.get("/proj/:id", getProjById);
+router.put("/proj/:id", updateProj);
+router.delete("/proj/:id", deleteProj);
+*/
+export default router;
