acc4f04089edd70bb4c2dc8f3bb58099a1705bf7
Max Kollmeyer <max.kollmeyer@gmail.com>
2025-11-27 22:29:50 -0500

    created project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(cors);
+
+//Routes
+
+//Error handling
+
+//Server running
+app.listen(port, () => {
+    console.log(`Server is running on localhost:${port}`);
+});
